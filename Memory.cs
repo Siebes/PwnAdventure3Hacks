2385ad7cad19a40bc26bf75175fafadc7e80b8e7
Ryan Siebert <ryan@siebert.dev>
2021-09-08 23:32:53 +1000

    Add project files.

---
+++ Memory.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security;
+using System.Text;
+
+namespace PwnAdventure
+{
+    class Memory
+    {
+        private static readonly long INVALID_HANDLE_VALUE = -1;
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct PROCESSENTRY32
+        {
+            public uint dwSize;
+            public uint cntUsage;
+            public uint th32ProcessID;
+            public IntPtr th32DefaultHeapID;
+            public uint th32ModuleID;
+            public uint cntThreads;
+            public uint th32ParentProcessID;
+            public int pcPriClassBase;
+            public uint dwFlags;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)] public string szExeFile;
+        };
+
+        [StructLayout(LayoutKind.Sequential, CharSet = System.Runtime.InteropServices.CharSet.Ansi)]
+        public struct MODULEENTRY32
+        {
+            internal uint dwSize;
+            internal uint th32ModuleID;
+            internal uint th32ProcessID;
+            internal uint GlblcntUsage;
+            internal uint ProccntUsage;
+            internal IntPtr modBaseAddr;
+            internal uint modBaseSize;
+            internal IntPtr hModule;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+            internal string szModule;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+            internal string szExePath;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct ModuleInformation
+        {
+            public IntPtr lpBaseOfDll;
+            public uint SizeOfImage;
+            public IntPtr EntryPoint;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct Module
+        {
+            public string moduleName;
+            public IntPtr baseAddress;
+            public uint size;
+        }
+
+        [Flags]
+        public enum ProcessAccessFlags : uint
+        {
+            All = 0x001F0FFF,
+            Terminate = 0x00000001,
+            CreateThread = 0x00000002,
+            VirtualMemoryOperation = 0x00000008,
+            VirtualMemoryRead = 0x00000010,
+            VirtualMemoryWrite = 0x00000020,
+            DuplicateHandle = 0x00000040,
+            CreateProcess = 0x000000080,
+            SetQuota = 0x00000100,
+            SetInformation = 0x00000200,
+            QueryInformation = 0x00000400,
+            QueryLimitedInformation = 0x00001000,
+            Synchronize = 0x00100000
+        }
+
+        [Flags]
+        private enum SnapshotFlags : uint
+        {
+            HeapList = 0x00000001,
+            Process = 0x00000002,
+            Thread = 0x00000004,
+            Module = 0x00000008,
+            Module32 = 0x00000010,
+            Inherit = 0x80000000,
+            All = 0x0000001F,
+            NoHeaps = 0x40000000
+        }
+
+        [Flags]
+        enum LoadLibraryFlags : uint
+        {
+            None = 0,
+            DONT_RESOLVE_DLL_REFERENCES = 0x00000001,
+            LOAD_IGNORE_CODE_AUTHZ_LEVEL = 0x00000010,
+            LOAD_LIBRARY_AS_DATAFILE = 0x00000002,
+            LOAD_LIBRARY_AS_DATAFILE_EXCLUSIVE = 0x00000040,
+            LOAD_LIBRARY_AS_IMAGE_RESOURCE = 0x00000020,
+            LOAD_LIBRARY_SEARCH_APPLICATION_DIR = 0x00000200,
+            LOAD_LIBRARY_SEARCH_DEFAULT_DIRS = 0x00001000,
+            LOAD_LIBRARY_SEARCH_DLL_LOAD_DIR = 0x00000100,
+            LOAD_LIBRARY_SEARCH_SYSTEM32 = 0x00000800,
+            LOAD_LIBRARY_SEARCH_USER_DIRS = 0x00000400,
+            LOAD_WITH_ALTERED_SEARCH_PATH = 0x00000008
+        }
+
+        internal enum ModuleFilter
+        {
+            ListModulesDefault = 0x0,
+            ListModules32Bit = 0x01,
+            ListModules64Bit = 0x02,
+            ListModulesAll = 0x03,
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr OpenProcess(ProcessAccessFlags processAccess, bool bInheritHandle, int processId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out] byte[] lpBuffer, int dwSize, out IntPtr lpNumberOfBytesRead);
+
+
+        [DllImport("kernel32.dll")]
+        public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [MarshalAs(UnmanagedType.AsAny)] object lpBuffer, Int32 nSize, out IntPtr lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll")]
+        static extern bool Process32First(IntPtr hSnapshot, ref PROCESSENTRY32 lppe);
+
+        [DllImport("kernel32.dll")]
+        static extern bool Process32Next(IntPtr hSnapshot, ref PROCESSENTRY32 lppe);
+
+        [DllImport("kernel32.dll")]
+        static extern bool Module32First(IntPtr hSnapshot, ref MODULEENTRY32 lpme);
+
+        [DllImport("kernel32.dll")]
+        static extern bool Module32Next(IntPtr hSnapshot, ref MODULEENTRY32 lpme);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
+        [SuppressUnmanagedCodeSecurity]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool CloseHandle(IntPtr hObject);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr CreateToolhelp32Snapshot(SnapshotFlags dwFlags, int th32ProcessID);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("psapi.dll", SetLastError = true)]
+        public static extern bool EnumProcessModules(IntPtr hProcess,
+[MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.U4)][In][Out] uint[] lphModule, uint cb, [MarshalAs(UnmanagedType.U4)] out uint lpcbNeeded);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr LoadLibraryEx(string lpFileName, IntPtr hReservedNull, LoadLibraryFlags dwFlags);
+
+        [DllImport("psapi.dll")]
+        public static extern bool EnumProcessModulesEx(IntPtr hProcess, [MarshalAs(UnmanagedType.LPArray, ArraySubType = UnmanagedType.U4)][In][Out] IntPtr[] lphModule, int cb, [MarshalAs(UnmanagedType.U4)] out int lpcbNeeded, uint dwFilterFlag);
+
+        [DllImport("psapi.dll")]
+        public static extern uint GetModuleFileNameEx(IntPtr hProcess, IntPtr hModule, [Out] StringBuilder lpBaseName, [In][MarshalAs(UnmanagedType.U4)] uint nSize);
+
+        [DllImport("psapi.dll", SetLastError = true)]
+        public static extern bool GetModuleInformation(IntPtr hProcess, IntPtr hModule, out ModuleInformation lpmodinfo, uint cb);
+
+        public static IntPtr GetModuleBaseAddress(Process process, string moduleName)
+        {
+            foreach (ProcessModule m in process.Modules)
+            {
+                if (m.ModuleName == moduleName)
+                {
+                    return m.BaseAddress;
+                }
+            }
+
+            throw new ApplicationException("Couldn't find Module Base Address \"" + moduleName + "\"");
+        }
+
+        public static IntPtr GetModuleBaseAddress(int processId, string moduleName)
+        {
+            IntPtr hSnap = CreateToolhelp32Snapshot(SnapshotFlags.All, processId);
+
+            if (hSnap.ToInt64() != INVALID_HANDLE_VALUE)
+            {
+                MODULEENTRY32 moduleEntry = new MODULEENTRY32();
+                moduleEntry.dwSize = (uint)Marshal.SizeOf(typeof(MODULEENTRY32));
+
+                if (Module32First(hSnap, ref moduleEntry))
+                {
+                    while (Module32Next(hSnap, ref moduleEntry))
+                    {
+                        if (moduleEntry.szModule.Equals(moduleName, StringComparison.OrdinalIgnoreCase))
+                        {
+                            CloseHandle(hSnap);
+                            return moduleEntry.modBaseAddr;
+                        }
+                    }
+                }
+            }
+            CloseHandle(hSnap);
+            Console.WriteLine("\nCouldn't find Module Base Address \"" + moduleName + "\"");
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+            Environment.Exit(1);
+            return IntPtr.Zero;
+        }
+
+        public static IntPtr FindDynamicMemoryAddress(IntPtr moduleBaseAddress, IntPtr ptr, int[] offsets)
+        {
+            byte[] buffer = new byte[IntPtr.Size];
+            foreach (int nextOffset in offsets)
+            {
+                ReadProcessMemory(moduleBaseAddress, ptr, buffer, buffer.Length, out var read);
+                // PwnAdventure is a 32bit game so pointer size is 4, use ToInt64 if 64 bit.
+                IntPtr nextOffsetPtr = new IntPtr(BitConverter.ToInt32(buffer, 0));
+                ptr = IntPtr.Add(nextOffsetPtr, nextOffset);
+            }
+
+            return ptr;
+        }
+
+        public static byte[] Serialize<T>(T data)
+        where T : struct
+        {
+            var formatter = new BinaryFormatter();
+            var stream = new MemoryStream();
+            formatter.Serialize(stream, data);
+            return stream.ToArray();
+        }
+
+
+        public static T Deserialize<T>(byte[] array)
+        where T : struct
+        {
+            var stream = new MemoryStream(array);
+            var formatter = new BinaryFormatter();
+            return (T)formatter.Deserialize(stream);
+        }
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace PwnAdventure
+{
+    class Program
+    {
+
+        private static readonly string PROCESS_NAME = "PwnAdventure3-Win32-Shipping";
+        private static readonly string MODULE_NAME = "GameLogic.dll";
+
+        private static readonly float SPEED = 0.08f;
+
+        static void Main(string[] args)
+        {
+            Process pwnAdventure3 = Process.GetProcessesByName(PROCESS_NAME)[0];
+            Console.WriteLine($"\nFound {PROCESS_NAME}. Process ID = ${pwnAdventure3.Id}");
+
+            IntPtr pwnAdventure3Address = getPwnAdventure3Address(pwnAdventure3);
+            IntPtr gameLogicAddress = getGameLogicAddress(pwnAdventure3);
+            IntPtr freezeAddress = getFreezeAddress(pwnAdventure3);
+            IntPtr locationAddress = getLocationAdress(pwnAdventure3Address, gameLogicAddress);
+            IntPtr viewAddress = getViewAddress(pwnAdventure3Address, gameLogicAddress);
+
+            var freezing = Task.Run(async () =>
+            {
+                for (; ; )
+                {
+                    await Task.Delay(10);
+                    unfreezeUser(pwnAdventure3Address, freezeAddress);
+                    await Task.Delay(10);
+                    freezeUser(pwnAdventure3Address, freezeAddress);
+                }
+            });
+
+            var moving = Task.Run(async () =>
+            {
+                for (; ; )
+                {
+                    move(pwnAdventure3Address, locationAddress, translateForward(pwnAdventure3Address, locationAddress, viewAddress));
+                }
+            });
+
+            Console.WriteLine("\nPress enter to quit");
+            Console.ReadKey();
+
+        }
+
+        static IntPtr getLocationAdress(IntPtr pwnAdventure3Address, IntPtr gameLogicAddress)
+        {
+            return Memory.FindDynamicMemoryAddress(pwnAdventure3Address, IntPtr.Add(gameLogicAddress, 0x00097D7C), new int[] { 0x1C, 0x4, 0x114, 0x90 });
+        }
+
+        static IntPtr getViewAddress(IntPtr pwnAdventure3Address, IntPtr gameLogicAddress)
+        {
+            return Memory.FindDynamicMemoryAddress(pwnAdventure3Address, IntPtr.Add(gameLogicAddress, 0x00097D7C), new int[] { 0x1C, 0x4, 0x400, 0x80 });
+        }
+
+        static Vector3 getCurrentLocation(IntPtr pwnAdventure3Address, IntPtr locationAddress)
+        {
+            int bytesToRead = (int)Marshal.SizeOf(typeof(Vector3));
+            byte[] buffer = new byte[bytesToRead];
+            Memory.ReadProcessMemory(pwnAdventure3Address, locationAddress, buffer, bytesToRead, out var read);
+
+            Vector3 location = new Vector3();
+            using (var inputStream = new MemoryStream(buffer))
+            {
+                using (var reader = new BinaryReader(inputStream))
+                {
+                    location.X = reader.ReadSingle();
+                    location.Y = reader.ReadSingle();
+                    location.Z = reader.ReadSingle();
+                }
+            }
+
+            return location;
+        }
+
+        static Quaternion getCurrentView(IntPtr pwnAdventure3Address, IntPtr viewAddress)
+        {
+            int bytesToRead = (int)Marshal.SizeOf(typeof(Quaternion));
+            byte[] buffer = new byte[bytesToRead];
+            Memory.ReadProcessMemory(pwnAdventure3Address, viewAddress, buffer, bytesToRead, out var read);
+
+            Quaternion view = new Quaternion();
+            using (var inputStream = new MemoryStream(buffer))
+            {
+                using (var reader = new BinaryReader(inputStream))
+                {
+                    view.X = reader.ReadSingle();
+                    view.Y = reader.ReadSingle();
+                    view.Z = reader.ReadSingle();
+                    view.W = reader.ReadSingle();
+
+                }
+            }
+            return view;
+        }
+
+        static void move(IntPtr pwnAdventure3Address, IntPtr locationPtr, Vector3 target)
+        {
+            // Console.WriteLine($"\nMoving to target {target}");
+            float[] targetLocation = new float[] { target.X, target.Y, target.Z };
+            var size = targetLocation.Length * 4;
+            Memory.WriteProcessMemory(pwnAdventure3Address, locationPtr, targetLocation, size, out _);
+        }
+
+        static Vector3 translateForward(IntPtr pwnAdventure3Address, IntPtr locationAddress, IntPtr viewAddress)
+        {
+            Vector3 currentLocation = getCurrentLocation(pwnAdventure3Address, locationAddress);
+            Quaternion currentView = getCurrentView(pwnAdventure3Address, viewAddress);
+            Vector3 translation = new Vector3();
+            translation.X = (2 * currentView.X * currentView.Z) - (2 * currentView.Y * currentView.W); // 2xz−2yw
+            translation.Y = (2 * currentView.Y * currentView.Z) + (2 * currentView.X * currentView.W); // 2yz+2xw
+            translation.Z = 1 - (2 * (currentView.X * currentView.X)) - (2 * (currentView.Y * currentView.Y)); //1−2x2−2y2]
+
+            translation.X = 1 - 2 * (currentView.Y * currentView.Y + currentView.Z * currentView.Z); // 1 - 2 * (y^2 + z^2)
+            translation.Y = 2 * (currentView.X * currentView.Y + currentView.W * currentView.Z); // 2 ( x * y + w * z)
+            translation.Z = 2 * (currentView.X * currentView.Z - currentView.W * currentView.Y); // 2 ( x * z - w * y )
+
+            translation.X = translation.X * SPEED;
+            translation.Y = translation.Y * SPEED;
+            translation.Z = translation.Z * SPEED;
+
+            currentLocation.X = currentLocation.X + translation.X;
+            currentLocation.Y = currentLocation.Y + translation.Y;
+            currentLocation.Z = currentLocation.Z + translation.Z;
+
+            return currentLocation;
+        }
+
+
+        static void freezeUser(IntPtr pwnAdventure3Address, IntPtr moveAddress)
+        {
+            byte[] freeze = new byte[] { 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90 };
+            Memory.WriteProcessMemory(pwnAdventure3Address, moveAddress, freeze, freeze.Length, out _);
+        }
+
+        static void unfreezeUser(IntPtr pwnAdventure3Address, IntPtr moveAddress)
+        {
+            byte[] unFreeze = new byte[] { 0x0F, 0x29, 0x86, 0x90, 0x00, 0x00, 0x00 };
+            Memory.WriteProcessMemory(pwnAdventure3Address, moveAddress, unFreeze, unFreeze.Length, out _);
+        }
+
+        static IntPtr getPwnAdventure3Address(Process pwnAdventure3)
+        {
+            return Memory.OpenProcess(Memory.ProcessAccessFlags.All, false, pwnAdventure3.Id);
+        }
+
+        static IntPtr getGameLogicAddress(Process pwnAdventure3)
+        {
+            return Memory.GetModuleBaseAddress(pwnAdventure3.Id, MODULE_NAME);
+        }
+
+        static IntPtr getFreezeAddress(Process pwnAdventure3)
+        {
+            return IntPtr.Add(pwnAdventure3.MainModule.BaseAddress, 0x008DB2D8);
+        }
+
+    }
+}
